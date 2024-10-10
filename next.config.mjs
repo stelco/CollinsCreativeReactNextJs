3352cdc100df9e54a2ac843adbb21401aa735d7f
stelco <stec74@gmail.com>
2024-10-10 11:08:31 +0100

    added domain to config for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-        ppr: 'incremental',
-      },
+  experimental: {
+    ppr: 'incremental',
+  },
+  images: {
+    domains: ['collins-creative-react-next-krq8613kz-steven-collins-projects.vercel.app'], // Add your domain here
+  },
+  // Other configurations
 };
 
 export default nextConfig;
