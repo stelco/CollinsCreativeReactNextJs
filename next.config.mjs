32b09b13858903e09f07b3e2e742bd132638ed20
stelco <stec74@gmail.com>
2024-10-25 13:20:06 +0100

    home page redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,16 @@
 const nextConfig = {
   experimental: {
     ppr: 'incremental',
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/portfolio/home',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
