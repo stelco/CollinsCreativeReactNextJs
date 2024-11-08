b893fdf93574d7cd4f1cc21d4b0fe56eae6a8e31
stelco <stec74@gmail.com>
2024-11-08 19:09:02 +0000

    added AI text

--- app/portfolio/data/home.tsx
+++ app/portfolio/data/home.tsx
@@ -16,7 +16,7 @@ const aboutPage =
     {
         title: 'About This Portfolio',
         heading: 'This is a React/Next.js app with Tailwind CSS',
-        value: 'This is a Next.js app with Tailwind CSS, TypeScript, GraphQl and Heroicons. It is a simple portfolio site that showcases my more recent technical and design skills.',
+        value: 'This is a Next.js app with Tailwind CSS, TypeScript, GraphQl and Heroicons. It is a simple portfolio site that showcases my more recent technical and design skills. Im also experimenting with new AI technologies and integrating them into the site.',
         value2: 'It is built using the Next.js tutorial Dashboard page as a boilerplate and the components and data have been customised for my portfolio pages. It uses a PostGres SQL database for the Dashboard data and local JSON data for the Skills. It is a work in progress and I will be adding more features and content in the future.',
         image: '/nextjs-logo.png'
     };
