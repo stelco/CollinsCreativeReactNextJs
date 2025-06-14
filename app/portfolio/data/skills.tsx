6d17e2973c609437bfebddc2db53030589f732d9
stelco <stec74@gmail.com>
2025-06-14 14:08:17 +0100

    june 25 updates

--- app/portfolio/data/skills.tsx
+++ app/portfolio/data/skills.tsx
@@ -1,123 +1,123 @@
 const skills = [
   {
-    id: 0,
-    name: "HTML",
-    color: "skill-advanced",
-    percentage: 100,
+    "id": 0,
+    "name": "HTML",
+    "color": "skill-advanced",
+    "percentage": 100
   },
   {
-    id: 1,
-    name: "CSS",
-    color: "skill-advanced",
-    percentage: 100,
+    "id": 1,
+    "name": "CSS/SASS",
+    "color": "skill-advanced",
+    "percentage": 100
   },
   {
-    id: 2,
-    name: "JavaScript",
-    color: "skill-advanced",
-    percentage: 80,
+    "id": 2,
+    "name": "JavaScript",
+    "color": "skill-advanced",
+    "percentage": 80
   },
   {
-    id: 3,
-    name: "TypeScript",
-    color: "skill-advanced",
-    percentage: 70,
+    "id": 3,
+    "name": "TypeScript",
+    "color": "skill-advanced",
+    "percentage": 70
   },
   {
-    id: 4,
-    name: "NodeJs",
-    color: "skill-intermediate",
-    percentage: 60,
+    "id": 4,
+    "name": "NodeJs",
+    "color": "skill-intermediate",
+    "percentage": 60
   },
   {
-    id: 5,
-    name: "VueJs",
-    color: "skill-advanced",
-    percentage: 100,
+    "id": 5,
+    "name": "VueJs",
+    "color": "skill-advanced",
+    "percentage": 100
   },
   {
-    id: 6,
-    name: "React",
-    color: "skill-intermediate",
-    percentage: 80,
+    "id": 6,
+    "name": "React",
+    "color": "skill-advanced",
+    "percentage": 100
   },
   {
-    id: 7,
-    name: "NPM/PNPM/Yarn",
-    color: "skill-intermediate",
+    "id": 7,
+    "name": "NPM/PNPM/Yarn",
+    "color": "skill-intermediate"
   },
   {
-    id: 8,
-    name: ".Net Core",
-    color: "skill-intermediate",
-    percentage: 50,
+    "id": 8,
+    "name": ".Net Core",
+    "color": "skill-intermediate",
+    "percentage": 50
   },
   {
-    id: 9,
-    name: "C#",
-    color: "skill-basic",
-    percentage: 50,
+    "id": 9,
+    "name": "C#",
+    "color": "skill-intermediate",
+    "percentage": 70
   },
   {
-    id: 10,
-    name: "MVC",
-    color: "skill-intermediate",
+    "id": 10,
+    "name": "MVC",
+    "color": "skill-intermediate"
   },
   {
-    id: 11,
-    name: "REST/Web API",
-    color: "skill-intermediate",
+    "id": 11,
+    "name": "REST/Web API",
+    "color": "skill-intermediate",
+    "percentage": 90
   },
   {
-    id: 12,
-    name: "SQL",
-    color: "skill-basic",
+    "id": 12,
+    "name": "SQL",
+    "color": "skill-basic"
   },
   {
-    id: 13,
-    name: "PostGresSQL",
-    color: "skill-intermediate",
-    percentage: 60,
+    "id": 13,
+    "name": "PostGresSQL",
+    "color": "skill-intermediate",
+    "percentage": 60
   },
   {
-    id: 14,
-    name: "PHP",
-    color: "skill-intermediate",
+    "id": 14,
+    "name": "PHP",
+    "color": "skill-intermediate"
   },
   {
-    id: 15,
-    name: "Angular",
-    color: "skill-intermediate",
-    percentage: 70,
+    "id": 15,
+    "name": "Angular",
+    "color": "skill-intermediate",
+    "percentage": 70
   },
   {
-    id: 16,
-    name: "CMS Development",
-    color: "skill-intermediate",
+    "id": 16,
+    "name": "CMS",
+    "color": "skill-advanced",
+    "percentage": 80
   },
   {
-    id: 17,
-    name: "UI/UX Design",
-    color: "skill-intermediate",
-    percentage: 70,
+    "id": 17,
+    "name": "UI/UX Design",
+    "color": "skill-intermediate",
+    "percentage": 70
   },
   {
-    id: 18,
-    name: "Azure DevOps",
-    color: "skill-intermediate",
-    percentage: 70,
+    "id": 18,
+    "name": "Azure DevOps",
+    "color": "skill-intermediate"
   },
   {
-    id: 19,
-    name: "BitBucket",
-    color: "skill-intermediate",
-    percentage: 60,
+    "id": 19,
+    "name": "BitBucket",
+    "color": "skill-intermediate"
   },
   {
-    id: 20,
-    name: "CI/CD",
-    color: "skill-basic",
-    percentage: 40,
+    "id": 20,
+    "name": "CI/CD",
+    "color": "skill-basic",
+    "percentage": 40
   }
 ];
 
