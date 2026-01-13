f4b1cb81dc5cfa6be0c3dbb85337b271528fba45
Steven Collins <stec74@gmail.com>
2026-01-13 21:32:48 +0000

    start call zindex fixauto renew token caching fix

--- app/components/ai/hume/voice/StartCall.tsx
+++ app/components/ai/hume/voice/StartCall.tsx
@@ -11,7 +11,7 @@ export default function StartCall({ configId, accessToken }: { configId?: string
     <AnimatePresence>
       {status.value !== "connected" ? (
         <motion.div
-          className={"fixed inset-0 p-4 flex items-center justify-center bg-background"}
+          className={"fixed inset-0 p-4 flex items-center justify-center bg-background z-[-1]"}
           initial="initial"
           animate="enter"
           exit="exit"
